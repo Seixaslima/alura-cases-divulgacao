ef6240fd40da31c68f1ee75f52bda5395ce71c50
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-08-22 12:11:53 -0300

    mudança completa de tsx para jsx por motivos didaticos

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFount() {
+  return (
+    <div>
+      <h1>Parece que você errou a pagina</h1>
+      <Link href="/">Voltar para Home Page</Link>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,18 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 
-interface ITitle {
-  children: ReactNode
-  as?: HTMLElementTagNameMap
-}
-
-function Title({ children, as = 'h1' }: ITitle) {
-  const Tag = as
+function Title({ children, as = "h1" }) {
+  const Tag = as;
   return (
     <>
       <Tag>{children}</Tag>
       <style jsx>{`
         ${Tag} {
-          color: red
+          color: red;
         }
       `}</style>
     </>
-  )
+  );
 }
 
 export default function HomePage() {
@@ -25,7 +20,6 @@ export default function HomePage() {
     <div className="banana">
       <Title>Alura Cases - Home</Title>
       <Link href="/faq">Ir para o FAQ</Link>
-
     </div>
-  )
+  );
 }
